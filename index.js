573ee3f813809f1033b8026841786219ef684244
shubhamkumar12-del <shubhamsid513@gmail.com>
2026-03-05 18:40:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const {v4:uuidv4} = require("uuid");
+const methodoverride = require("method-override");
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
+
+app.set("view enjine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+let posts = [
+    {
+        id:uuidv4(),
+        username:"shubham kumar",
+        content:"i love my india",
+    },
+    {
+        id:uuidv4(),
+        username:"priya paul",
+        content:"she is very hardworking",
+    },
+    {
+        id:uuidv4(),
+        username:"satyam",
+        content:"he is lazy",
+    },
+];
+
+app.get("/posts",(req,res) =>{
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res) =>{
+    let{username, content} = req.body;
+    let id = uuidv4();
+    posts.push({username, content,id});
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let{id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newcontent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newcontent;
+    // console.log(post);
+    res.redirect("/posts");
+    
+});
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let{id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post});
+});
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+     posts = posts.filter((p) => id !==p.id);
+     res.redirect("/posts");
+});
+app.listen(port, (req,res) =>{
+    console.log("listening server",8080);
+});
